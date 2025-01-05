0a8854fd77ed3057fccfd88a956306624415c9b7
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-01-05 14:27:40 +0800

    file_test.go: add TestFileInstance, rename variable in TestFile.

--- loader/file_test.go
+++ loader/file_test.go
@@ -1,6 +1,8 @@
 package loader
 
 import (
+	"path/filepath"
+	"runtime"
 	"testing"
 
 	"github.com/davecgh/go-spew/spew"
@@ -11,16 +13,65 @@ const testFilePath = "C:\\Windows\\System32\\cmd.exe"
 
 func TestFile(t *testing.T) {
 	t.Run("common", func(t *testing.T) {
-		file := NewFile(testFilePath)
+		image := NewFile(testFilePath)
 
-		config, err := file.Encode()
+		config, err := image.Encode()
 		require.NoError(t, err)
 
 		spew.Dump(config)
 	})
 
 	t.Run("mode", func(t *testing.T) {
-		file := NewFile(testFilePath)
-		require.Equal(t, ModeFile, file.Mode())
+		image := NewFile(testFilePath)
+		require.Equal(t, ModeFile, image.Mode())
+	})
+}
+
+func TestFileInstance(t *testing.T) {
+	opts := &Options{
+		ImageName:    "test.exe",
+		CommandLine:  "-p1 123 -p2 \"hello\"",
+		WaitMain:     true,
+		AllowSkipDLL: true,
+	}
+
+	t.Run("x86", func(t *testing.T) {
+		if runtime.GOOS != "windows" || runtime.GOARCH != "386" {
+			return
+		}
+
+		for _, item := range images {
+			path, err := filepath.Abs(filepath.Join("../test/image/x86", item.path))
+			require.NoError(t, err)
+			image := NewFile(path)
+			opts.WaitMain = item.wait
+
+			inst, err := CreateInstance(testTplX86, 32, image, opts)
+			require.NoError(t, err)
+
+			addr := loadShellcode(t, inst)
+			ret, _, _ := syscallN(addr)
+			require.NotEqual(t, uintptr(0), ret)
+		}
+	})
+
+	t.Run("x64", func(t *testing.T) {
+		if runtime.GOOS != "windows" || runtime.GOARCH != "amd64" {
+			return
+		}
+
+		for _, item := range images {
+			path, err := filepath.Abs(filepath.Join("../test/image/x64", item.path))
+			require.NoError(t, err)
+			image := NewFile(path)
+			opts.WaitMain = item.wait
+
+			inst, err := CreateInstance(testTplX64, 64, image, opts)
+			require.NoError(t, err)
+
+			addr := loadShellcode(t, inst)
+			ret, _, _ := syscallN(addr)
+			require.NotEqual(t, uintptr(0), ret)
+		}
 	})
 }
