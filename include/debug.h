8f4f12a60c27fb4290462026138e052e0bbacbf6
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-03-26 19:52:37 +0800

    debug.h: add kernel object name for debug, add comment.

--- include/debug.h
+++ include/debug.h
@@ -4,6 +4,19 @@
 #include "build.h"
 #include "c_types.h"
 
+#ifdef RELEASE_MODE
+    #define NAME_LDR_MUTEX_GLOBAL NULL
+    #define NAME_LDR_MUTEX_STATUS NULL
+#else
+#ifdef _WIN64
+    #define NAME_LDR_MUTEX_GLOBAL "LDR_Global_x64"
+    #define NAME_LDR_MUTEX_STATUS "LDR_Status_x64"
+#elif _WIN32
+    #define NAME_LDR_MUTEX_GLOBAL "LDR_Global_x86"
+    #define NAME_LDR_MUTEX_STATUS "LDR_Status_x86"
+#endif
+#endif // RELEASE_MODE
+
 #ifndef RELEASE_MODE
 
 bool InitDebugger();
@@ -16,6 +29,6 @@ void dbg_log(char* mod, char* fmt, ...);
 
 #define dbg_log(mod, fmt, ...)
 
-#endif
+#endif // RELEASE_MODE
 
 #endif // DEBUG_H
