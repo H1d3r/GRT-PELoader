da993ccbfdc5a9f5787fa9c4eb8b9aecc9f20aae
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-04-03 17:14:41 +0800

    adjust the command for unify the order of x86 and x64.

--- Build.bat
+++ Build.bat
@@ -16,26 +16,26 @@ rd /S /Q "Release"
 rd /S /Q "x64"
 
 echo ======================== generate ========================
-MSBuild.exe GRT-PELoader.sln /t:builder /p:Configuration=Release /p:Platform=x64
 MSBuild.exe GRT-PELoader.sln /t:builder /p:Configuration=Release /p:Platform=x86
-MSBuild.exe GRT-PELoader.sln /t:cutter /p:Configuration=Release /p:Platform=x64
+MSBuild.exe GRT-PELoader.sln /t:builder /p:Configuration=Release /p:Platform=x64
 MSBuild.exe GRT-PELoader.sln /t:cutter /p:Configuration=Release /p:Platform=x86
+MSBuild.exe GRT-PELoader.sln /t:cutter /p:Configuration=Release /p:Platform=x64
 
 echo =============== extract PE Loader shellcode ==============
 del /S /Q dist
 
 cd builder
-echo --------extract shellcode for x64--------
-"..\x64\Release\builder.exe"
 echo --------extract shellcode for x86--------
 "..\Release\builder.exe"
+echo --------extract shellcode for x64--------
+"..\x64\Release\builder.exe"
 cd ..
 
 cd cutter
-echo ----------cut PE Loader for x64----------
-"..\x64\Release\cutter.exe"
 echo ----------cut PE Loader for x86----------
 "..\Release\cutter.exe"
+echo ----------cut PE Loader for x64----------
+"..\x64\Release\cutter.exe"
 cd ..
 
 echo =================== clean output files ===================
