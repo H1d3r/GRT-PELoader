cb0904c0a7d00410ac1f149dc2a4a5ffb26b7b4e
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-05-31 14:05:06 +0800

    loader.go: add PELoaderM.lock and unlock, copy PELoaderM.

--- loader/loader.go
+++ loader/loader.go
@@ -7,6 +7,8 @@ import (
 	"syscall"
 	"unsafe"
 
+	"golang.org/x/sys/windows"
+
 	"github.com/RSSU-Shellcode/Gleam-RT/runtime"
 )
 
@@ -75,6 +77,9 @@ type PELoaderM struct {
 	// main thread return value or argument about call ExitProcess.
 	ExitCode uint
 
+	// runtime mutex, need lock it before call some loader methods.
+	runtimeMu uintptr
+
 	// get export method address if PE image is a DLL.
 	getProc uintptr
 
@@ -102,7 +107,9 @@ func InitPELoader(addr uintptr, runtime *gleamrt.RuntimeM, config *Config) (*PEL
 	if ptr == null {
 		return nil, fmt.Errorf("failed to initialize PE Loader: 0x%X", err)
 	}
-	return NewPELoader(ptr), nil
+	// copy memory for prevent runtime encrypt memory page when call loader method
+	loader := *(NewPELoader(ptr))
+	return &loader, nil
 }
 
 // GetProcAddress is used to get procedure address by name.
@@ -119,7 +126,7 @@ func (ldr *PELoaderM) GetProcAddress(name string) (uintptr, error) {
 }
 
 // Execute is used to execute exe or call DllMain with DLL_PROCESS_ATTACH.
-// It can call multi times.
+// It can call multi times with Exit.
 func (ldr *PELoaderM) Execute() error {
 	en, _, _ := syscall.SyscallN(ldr.execute)
 	if en != noError {
@@ -129,8 +136,10 @@ func (ldr *PELoaderM) Execute() error {
 }
 
 // Exit is used to exit exe or call DllMain with DLL_PROCESS_DETACH.
-// It can call multi times.
+// It can call multi times with Execute.
 func (ldr *PELoaderM) Exit(code uint) error {
+	ldr.lock()
+	defer ldr.unlock()
 	en, _, _ := syscall.SyscallN(ldr.exit, uintptr(code))
 	if en != noError {
 		return &errno{method: "Exit", errno: en}
@@ -141,9 +150,27 @@ func (ldr *PELoaderM) Exit(code uint) error {
 // Destroy is used to destroy all resource about PE loader.
 // It can only call one time.
 func (ldr *PELoaderM) Destroy() error {
+	ldr.lock()
+	// defer ldr.unlock() runtime will close the mutex
 	en, _, _ := syscall.SyscallN(ldr.destroy)
 	if en != noError {
 		return &errno{method: "Destroy", errno: en}
 	}
 	return nil
+}
+
+func (ldr *PELoaderM) lock() {
+	hMutex := windows.Handle(ldr.runtimeMu)
+	_, err := windows.WaitForSingleObject(hMutex, windows.INFINITE)
+	if err != nil {
+		panic(fmt.Sprintf("failed to lock runtime mutex: %s", err))
+	}
+}
+
+func (ldr *PELoaderM) unlock() {
+	hMutex := windows.Handle(ldr.runtimeMu)
+	err := windows.ReleaseMutex(hMutex)
+	if err != nil {
+		panic(fmt.Sprintf("failed to release runtime mutex: %s", err))
+	}
 }
