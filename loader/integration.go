24714197b1ef4b4efbf951869b49e7414b54bdd1
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-06-01 14:31:05 +0800

    integration.go: add LoadInMemoryEXE and LoadInMemoryDLL.

---
+++ loader/integration.go
@@ -0,0 +1,92 @@
+//go:build windows
+
+package loader
+
+import (
+	"bytes"
+	"debug/pe"
+	"errors"
+	"fmt"
+	"syscall"
+	"unsafe"
+
+	"golang.org/x/sys/windows"
+)
+
+// LoadInMemoryEXE is used to load an unmanaged exe image to memory.
+func LoadInMemoryEXE(template, image []byte, opts *Options) (*PELoaderM, error) {
+	loader, err := loadInstance(template, image, opts, false)
+	if err != nil {
+		return nil, err
+	}
+	err = loader.Execute()
+	if err != nil {
+		return nil, fmt.Errorf("failed to execute image: %s", err)
+	}
+	return loader, nil
+}
+
+// LoadInMemoryDLL is used to load an unmanaged dll image to memory.
+func LoadInMemoryDLL(template, image []byte, opts *Options) (*PELoaderM, error) {
+	loader, err := loadInstance(template, image, opts, true)
+	if err != nil {
+		return nil, err
+	}
+	err = loader.Execute()
+	if err != nil {
+		return nil, fmt.Errorf("failed to call dll_main: %s", err)
+	}
+	return loader, nil
+}
+
+func loadInstance(template, image []byte, opts *Options, isDLL bool) (*PELoaderM, error) {
+	peFile, err := pe.NewFile(bytes.NewReader(image))
+	if err != nil {
+		return nil, err
+	}
+	if isDLL && (peFile.Characteristics&pe.IMAGE_FILE_DLL) == 0 {
+		return nil, errors.New("pe image is not a dll")
+	}
+	var arch int
+	switch peFile.Machine {
+	case pe.IMAGE_FILE_MACHINE_AMD64:
+		arch = 64
+	case pe.IMAGE_FILE_MACHINE_I386:
+		arch = 32
+	default:
+		return nil, errors.New("unknown pe image architecture type")
+	}
+	// overwrite some options
+	if opts == nil {
+		opts = new(Options)
+	}
+	options := *opts
+	options.Runtime.NotAdjustProtect = true
+	options.Runtime.TrackCurrentThread = false
+	// create instance
+	instance, err := CreateInstance(template, arch, NewEmbed(image), &options)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create instance: %s", err)
+	}
+	// prepare memory page for write instance
+	size := uintptr(len(instance))
+	mType := uint32(windows.MEM_COMMIT | windows.MEM_RESERVE)
+	mProtect := uint32(windows.PAGE_READWRITE)
+	scAddr, err := windows.VirtualAlloc(0, size, mType, mProtect)
+	if err != nil {
+		return nil, fmt.Errorf("failed to alloc memory for instance: %s", err)
+	}
+	var old uint32
+	err = windows.VirtualProtect(scAddr, size, windows.PAGE_EXECUTE_READWRITE, &old)
+	if err != nil {
+		return nil, fmt.Errorf("failed to change memory protect: %s", err)
+	}
+	dst := unsafe.Slice((*byte)(unsafe.Pointer(scAddr)), size) // #nosec
+	copy(dst, instance)
+	// load instance
+	ptr, _, err := syscall.SyscallN(scAddr)
+	if ptr == null {
+		return nil, fmt.Errorf("failed to load instance: 0x%X", err)
+	}
+	return NewPELoader(ptr), nil
+}
