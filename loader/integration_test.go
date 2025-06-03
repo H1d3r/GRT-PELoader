56a8f31c9807c3cd032d1f54aac2ede7029b6090
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-06-03 14:16:58 +0800

    integration_test.go: improve code about select image, update TestLoadInMemoryEXE.

--- loader/integration_test.go
+++ loader/integration_test.go
@@ -11,108 +11,87 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+var (
+	testTemplate []byte
+	testImage    []byte
+)
+
+func init() {
+	var err error
+	switch runtime.GOARCH {
+	case "386":
+		testTemplate = testLDRx86
+		testImage, err = os.ReadFile("../test/image/x86/rust_msvc.exe")
+	case "amd64":
+		testTemplate = testLDRx64
+		testImage, err = os.ReadFile("../test/image/x64/rust_msvc.exe")
+	default:
+		panic("unsupported architecture")
+	}
+	if err != nil {
+		panic(err)
+	}
+}
+
 func TestLoadInMemoryEXE(t *testing.T) {
 	t.Run("wait exit", func(t *testing.T) {
-		var (
-			template []byte
-			image    []byte
-			err      error
-		)
-		switch runtime.GOARCH {
-		case "386":
-			template = testLDRx86
-			image, err = os.ReadFile("../test/image/x86/rust_msvc.exe")
-		case "amd64":
-			template = testLDRx64
-			image, err = os.ReadFile("../test/image/x64/rust_msvc.exe")
-		default:
-			t.Fatal("unsupported architecture")
-		}
-		require.NoError(t, err)
-
 		opts := Options{
-			WaitMain: true,
+			WaitMain:       true,
+			NotStopRuntime: false,
 		}
-		PELoaderM, err := LoadInMemoryEXE(template, image, &opts)
+		PELoaderM, err := LoadInMemoryEXE(testTemplate, testImage, &opts)
 		require.NoError(t, err)
 		require.Nil(t, PELoaderM)
 	})
 
 	t.Run("not wait exit", func(t *testing.T) {
-		var (
-			template []byte
-			image    []byte
-			err      error
-		)
-		switch runtime.GOARCH {
-		case "386":
-			template = testLDRx86
-			image, err = os.ReadFile("../test/image/x86/go.exe")
-		case "amd64":
-			template = testLDRx64
-			image, err = os.ReadFile("../test/image/x64/go.exe")
-		default:
-			t.Fatal("unsupported architecture")
-		}
-		require.NoError(t, err)
-
 		opts := Options{
-			CommandLine: "-kick 20",
-			WaitMain:    false,
+			CommandLine:    "-kick 20",
+			WaitMain:       false,
+			NotStopRuntime: true,
 		}
-		PELoaderM, err := LoadInMemoryEXE(template, image, &opts)
+		PELoaderM, err := LoadInMemoryEXE(testTemplate, testImage, &opts)
 		require.NoError(t, err)
 
-		time.Sleep(3 * time.Second)
+		time.Sleep(4 * time.Second)
 
 		err = PELoaderM.Exit(0)
 		require.NoError(t, err)
-		require.Zero(t, PELoaderM.ExitCode)
+		require.Zero(t, PELoaderM.ExitCode())
+
+		err = PELoaderM.Execute()
+		require.NoError(t, err)
+
+		time.Sleep(2 * time.Second)
 
 		err = PELoaderM.Destroy()
 		require.NoError(t, err)
 	})
 
 	t.Run("restart", func(t *testing.T) {
-		var (
-			template []byte
-			image    []byte
-			err      error
-		)
-		switch runtime.GOARCH {
-		case "386":
-			template = testLDRx86
-			image, err = os.ReadFile("../test/image/x86/go.exe")
-		case "amd64":
-			template = testLDRx64
-			image, err = os.ReadFile("../test/image/x64/go.exe")
-		default:
-			t.Fatal("unsupported architecture")
-		}
-		require.NoError(t, err)
-
 		opts := Options{
-			CommandLine: "-kick 20",
-			WaitMain:    false,
+			CommandLine:    "-kick 20",
+			WaitMain:       false,
+			NotStopRuntime: true,
 		}
-		PELoaderM, err := LoadInMemoryEXE(template, image, &opts)
+		PELoaderM, err := LoadInMemoryEXE(testTemplate, testImage, &opts)
 		require.NoError(t, err)
 
-		time.Sleep(3 * time.Second)
+		time.Sleep(2 * time.Second)
 
 		err = PELoaderM.Exit(0)
 		require.NoError(t, err)
-		require.Zero(t, PELoaderM.ExitCode)
+		require.Zero(t, PELoaderM.ExitCode())
 
 		for i := 0; i < 3; i++ {
 			err = PELoaderM.Execute()
 			require.NoError(t, err)
 
-			time.Sleep(3 * time.Second)
+			time.Sleep(2 * time.Second)
 
-			err = PELoaderM.Exit(1)
+			err = PELoaderM.Exit(uint(i) + 123)
 			require.NoError(t, err)
-			require.Equal(t, uint(1), PELoaderM.ExitCode)
+			require.Equal(t, uint(i)+123, PELoaderM.ExitCode())
 		}
 
 		err = PELoaderM.Destroy()
