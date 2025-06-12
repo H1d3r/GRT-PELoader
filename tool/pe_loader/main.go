e1a7b0d9da80cadbf1ae1224790d86cca88fcef7
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-06-12 18:27:45 +0800

    pe_loader/main.go: finish simple tool about process template.

---
+++ tool/pe_loader/main.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"bytes"
+	"debug/pe"
+	"flag"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/RSSU-Shellcode/GRT-Develop/option"
+	"github.com/RSSU-Shellcode/GRT-PELoader/loader"
+)
+
+var (
+	tplDir string
+	pePath string
+
+	options loader.Options
+)
+
+func init() {
+	flag.StringVar(&tplDir, "tpl", "", "set custom shellcode templates directory")
+	flag.StringVar(&pePath, "pe", "", "set the input PE image file path")
+	flag.StringVar(&options.ImageName, "in", "", "set the image name about command line")
+	flag.StringVar(&options.CommandLine, "cmd", "", "set command line for exe")
+	flag.BoolVar(&options.WaitMain, "wait", false, "wait for shellcode to exit")
+	flag.BoolVar(&options.AllowSkipDLL, "skip-dll", false, "allow skip DLL if failed to load")
+	flag.BoolVar(&options.IgnoreStdIO, "silent", false, "ignore input/output about console")
+	flag.BoolVar(&options.NotStopRuntime, "nsr", false, "not stop runtime when call ExitProcess")
+	option.Flag(&options.Runtime)
+	flag.Parse()
+}
+
+func main() {
+	if pePath == "" {
+		flag.Usage()
+		return
+	}
+
+	fmt.Println("[WARNING]")
+	fmt.Println("This program is only for test PE image is compatible")
+
+	// load custom loader template
+	var (
+		ldrX64 []byte
+		ldrX86 []byte
+	)
+	if tplDir != "" {
+		var err error
+		fmt.Println("load custom PE Loader templates")
+		ldrX64, err = os.ReadFile(filepath.Join(tplDir, "PELoader_x64.bin")) // #nosec
+		checkError(err)
+		ldrX86, err = os.ReadFile(filepath.Join(tplDir, "PELoader_x86.bin")) // #nosec
+		checkError(err)
+	}
+
+	fmt.Println("parse PE image file")
+	peData, err := os.ReadFile(pePath) // #nosec
+	checkError(err)
+	peFile, err := pe.NewFile(bytes.NewReader(peData))
+	checkError(err)
+	var arch string
+	switch peFile.Machine {
+	case pe.IMAGE_FILE_MACHINE_I386:
+		arch = "386"
+		fmt.Println("image architecture: x86")
+	case pe.IMAGE_FILE_MACHINE_AMD64:
+		arch = "amd64"
+		fmt.Println("image architecture: x64")
+	default:
+		fmt.Println("unknown pe image architecture type")
+		return
+	}
+
+	// select shellcode template
+	var template []byte
+	switch arch {
+	case "386":
+		template = ldrX86
+		fmt.Println("select template for x86")
+	case "amd64":
+		template = ldrX64
+		fmt.Println("select template for x64")
+	default:
+		fmt.Println("unknown template architecture")
+		return
+	}
+	if len(template) > 0 {
+		options.Template = template
+	}
+
+	if peFile.Characteristics&pe.IMAGE_FILE_DLL == 0 {
+		instance, err := loader.LoadInMemoryEXE(peData, &options)
+		checkError(err)
+	} else {
+		instance, err := loader.LoadInMemoryDLL(peData, &options)
+		checkError(err)
+	}
+}
+
+func checkError(err error) {
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
