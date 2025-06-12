751951f3d96dea23ec160a657dc998328603bd27
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-06-12 16:32:45 +0800

    integration.go: add methods about process standard handle pipe.

--- loader/integration.go
+++ loader/integration.go
@@ -8,6 +8,8 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"os"
+	"sync"
 	"syscall"
 	"unsafe"
 
@@ -21,9 +23,17 @@ type Instance struct {
 	instAddr uintptr
 	instData []byte
 
-	stdInputFile  io.WriteCloser
-	stdOutputFile io.ReadCloser
-	stdErrorFile  io.ReadCloser
+	stdInputR  *os.File
+	stdInputW  *os.File
+	stdOutputR *os.File
+	stdOutputW *os.File
+	stdErrorR  *os.File
+	stdErrorW  *os.File
+
+	sameOutErr bool
+	outErrMu   sync.Mutex
+
+	wg sync.WaitGroup
 }
 
 // LoadInMemoryEXE is used to load an unmanaged exe image to memory.
@@ -53,11 +63,12 @@ func loadInstance(image []byte, opts *Options, isDLL bool) (*Instance, error) {
 	default:
 		return nil, errors.New("unknown pe image architecture type")
 	}
+	// copy options for overwrite
 	if opts == nil {
 		opts = new(Options)
 	}
 	options := *opts
-	// process pipe
+	// process pipe for standard handle
 	instance := Instance{}
 	err = instance.startPipe(&options)
 	if err != nil {
@@ -100,44 +111,70 @@ func loadInstance(image []byte, opts *Options, isDLL bool) (*Instance, error) {
 }
 
 func (inst *Instance) startPipe(options *Options) error {
-	// var (
-	// 	stdInput  = opts.StdInput
-	// 	stdOutput = opts.StdOutput
-	// 	stdError  = opts.StdError
-	// )
-	// var (
-	// 	stdInputFile  io.WriteCloser
-	// 	stdOutputFile io.ReadCloser
-	// 	stdErrorFile  io.ReadCloser
-	// )
-	// if opts.StdInputFile != nil {
-	// 	r, w, err := os.Pipe()
-	// 	if err != nil {
-	// 		return nil, err
-	// 	}
-	// 	stdInput = uint64(r.Fd())
-	// 	stdInputFile = w
-	// }
-	// if opts.StdOutputFile != nil {
-	// 	r, w, err := os.Pipe()
-	// 	if err != nil {
-	// 		return nil, err
-	// 	}
-	// 	stdOutput = uint64(w.Fd())
-	// 	stdOutputFile = r
-	// }
-	// if opts.StdErrorFile != nil {
-	// 	r, w, err := os.Pipe()
-	// 	if err != nil {
-	// 		return nil, err
-	// 	}
-	// 	stdError = uint64(w.Fd())
-	// 	stdErrorFile = r
-	// }
+	if options.IgnoreStdIO {
+		return nil
+	}
+	if options.Stdout != nil && options.Stdout == options.Stderr {
+		inst.sameOutErr = true
+	}
 
-	// options.StdInput
-	// options.StdOutput
-	// options.StdError
+	return nil
+}
+
+func (inst *Instance) startStdinPipe(options *Options) error {
+	if options.StdInput != 0 || options.Stdin == nil {
+		return nil
+	}
+	r, w, err := os.Pipe()
+	if err != nil {
+		return fmt.Errorf("failed to create pipe for stdin: %s", err)
+	}
+	options.StdInput = uint64(r.Fd())
+	inst.stdInputR = r
+	inst.stdInputW = w
+	inst.wg.Add(1)
+	go func() {
+		defer inst.wg.Done()
+		_, _ = io.Copy(w, options.Stdin)
+	}()
+	return nil
+}
+
+func (inst *Instance) startStdoutPipe(options *Options) error {
+	if options.StdOutput != 0 || options.Stdout == nil {
+		return nil
+	}
+	r, w, err := os.Pipe()
+	if err != nil {
+		return fmt.Errorf("failed to create pipe for stdout: %s", err)
+	}
+	options.StdOutput = uint64(w.Fd())
+	inst.stdOutputR = r
+	inst.stdOutputW = w
+	inst.wg.Add(1)
+	go func() {
+		defer inst.wg.Done()
+		_, _ = io.Copy(options.Stdout, r)
+	}()
+	return nil
+}
+
+func (inst *Instance) startStderrPipe(options *Options) error {
+	if options.StdInput != 0 || options.Stdin == nil {
+		return nil
+	}
+	r, w, err := os.Pipe()
+	if err != nil {
+		return fmt.Errorf("failed to create pipe for stderr: %s", err)
+	}
+	options.StdError = uint64(w.Fd())
+	inst.stdErrorR = r
+	inst.stdErrorW = w
+	inst.wg.Add(1)
+	go func() {
+		defer inst.wg.Done()
+		_, _ = io.Copy(options.Stderr, r)
+	}()
 	return nil
 }
 
