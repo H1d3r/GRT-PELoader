103371ab4f641ac1ba78afe42b8d5084c4217683
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-06-14 23:55:50 +0800

    integration_test.go: add sub tests about process the pipe.

--- loader/integration_test.go
+++ loader/integration_test.go
@@ -3,6 +3,8 @@
 package loader
 
 import (
+	"bytes"
+	"fmt"
 	"os"
 	"runtime"
 	"testing"
@@ -11,20 +13,15 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-var (
-	testTemplate []byte
-	testImage    []byte
-)
+var testImage []byte
 
 func init() {
 	var err error
 	switch runtime.GOARCH {
 	case "386":
-		testTemplate = testLDRx86
-		testImage, err = os.ReadFile("../test/image/x86/rust_msvc.exe")
+		testImage, err = os.ReadFile("../test/image/x86/msvc.exe")
 	case "amd64":
-		testTemplate = testLDRx64
-		testImage, err = os.ReadFile("../test/image/x64/rust_msvc.exe")
+		testImage, err = os.ReadFile("../test/image/x64/go.exe")
 	default:
 		panic("unsupported architecture")
 	}
@@ -34,14 +31,70 @@ func init() {
 }
 
 func TestLoadInMemoryEXE(t *testing.T) {
-	t.Run("wait exit", func(t *testing.T) {
+	t.Run("common", func(t *testing.T) {
 		opts := Options{
-			WaitMain:       true,
-			NotStopRuntime: false,
+			CommandLine: "-kick 20",
 		}
-		PELoaderM, err := LoadInMemoryEXE(testTemplate, testImage, &opts)
+		instance, err := LoadInMemoryEXE(testImage, &opts)
 		require.NoError(t, err)
-		require.Nil(t, PELoaderM)
+
+		go func() {
+			time.Sleep(5 * time.Second)
+			err = instance.Free()
+			require.NoError(t, err)
+		}()
+
+		err = instance.Run()
+		require.NoError(t, err)
+	})
+
+	t.Run("with different output error", func(t *testing.T) {
+		stdout := new(bytes.Buffer)
+		stderr := new(bytes.Buffer)
+
+		opts := Options{
+			CommandLine: "-kick 20",
+			Stdout:      stdout,
+			Stderr:      stderr,
+		}
+		instance, err := LoadInMemoryEXE(testImage, &opts)
+		require.NoError(t, err)
+
+		go func() {
+			time.Sleep(5 * time.Second)
+			err = instance.Free()
+			require.NoError(t, err)
+		}()
+
+		err = instance.Run()
+		require.NoError(t, err)
+
+		fmt.Println("stdout:\n", stdout)
+		fmt.Println("stderr:\n", stderr)
+	})
+
+	t.Run("with same output error", func(t *testing.T) {
+		output := new(bytes.Buffer)
+
+		opts := Options{
+			CommandLine: "-kick 20",
+			Stdout:      output,
+			Stderr:      output,
+		}
+		instance, err := LoadInMemoryEXE(testImage, &opts)
+		require.NoError(t, err)
+
+		go func() {
+			time.Sleep(5 * time.Second)
+			err = instance.Free()
+			require.NoError(t, err)
+		}()
+
+		err = instance.Run()
+		require.NoError(t, err)
+
+		fmt.Println("stdout:\n", output)
+		fmt.Println("stderr:\n", output)
 	})
 
 	t.Run("not wait exit", func(t *testing.T) {
@@ -50,7 +103,7 @@ func TestLoadInMemoryEXE(t *testing.T) {
 			WaitMain:       false,
 			NotStopRuntime: true,
 		}
-		PELoaderM, err := LoadInMemoryEXE(testTemplate, testImage, &opts)
+		PELoaderM, err := LoadInMemoryEXE(testImage, &opts)
 		require.NoError(t, err)
 
 		time.Sleep(4 * time.Second)
@@ -74,7 +127,7 @@ func TestLoadInMemoryEXE(t *testing.T) {
 			WaitMain:       false,
 			NotStopRuntime: true,
 		}
-		PELoaderM, err := LoadInMemoryEXE(testTemplate, testImage, &opts)
+		PELoaderM, err := LoadInMemoryEXE(testImage, &opts)
 		require.NoError(t, err)
 
 		time.Sleep(2 * time.Second)
@@ -82,6 +135,8 @@ func TestLoadInMemoryEXE(t *testing.T) {
 		err = PELoaderM.Exit(0)
 		require.NoError(t, err)
 		require.Zero(t, PELoaderM.ExitCode())
+
+		time.Sleep(time.Hour)
 
 		for i := 0; i < 3; i++ {
 			err = PELoaderM.Execute()
