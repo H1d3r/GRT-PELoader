e3ad48cfca1f8814ea4f9cb7e56e95cffdf63043
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-06-19 15:07:52 +0800

    test.bat: add @echo off, build and test example for metrics.

--- example/test.bat
+++ example/test.bat
@@ -1,11 +1,15 @@
+@echo off
+
 go build -v -trimpath -o GRT-PELoader.exe ../tool/pe_loader/main.go
 
 go build -v -trimpath -ldflags "-s -w" -o argument.exe   argument/main.go
 go build -v -trimpath -ldflags "-s -w" -o im_storage.exe im_storage/main.go
+go build -v -trimpath -ldflags "-s -w" -o metrics.exe    metrics/main.go
 go build -v -trimpath -ldflags "-s -w" -o sleep.exe      sleep/main.go
 
 GRT-PELoader.exe -pe argument.exe
 GRT-PELoader.exe -pe im_storage.exe
+GRT-PELoader.exe -pe metrics.exe
 GRT-PELoader.exe -pe sleep.exe
 
 del /Q *.exe
