98f407eb8e1802715476d4acbdfb7a8dc8be741a
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-06-19 14:43:58 +0800

    im_storage/main.go: add example for IMS_Delete and IMS_DeleteAll.

--- example/im_storage/main.go
+++ example/im_storage/main.go
@@ -24,6 +24,8 @@ func main() {
 	setValue()
 	getValue()
 	getPointer()
+	deleteVal()
+	deleteAll()
 }
 
 func setValue() {
@@ -31,7 +33,7 @@ func setValue() {
 
 	data := []byte{0x01, 0x02, 0x03, 0x04}
 
-	id := uint32(0) // CommandLineA
+	id := int32(0)
 	ret, _, _ := SetValue.Call(
 		uintptr(id), uintptr(unsafe.Pointer(&data[0])), uintptr(len(data)),
 	) // #nosec
@@ -44,7 +46,7 @@ func setValue() {
 func getValue() {
 	GetValue := GleamRT.MustFindProc("IMS_GetValue")
 
-	id := uint32(0) // CommandLineA
+	id := int32(0)
 	var size uint
 	ret, _, _ := GetValue.Call(
 		uintptr(id), 0, uintptr(unsafe.Pointer(&size)),
@@ -70,5 +72,53 @@ func getValue() {
 }
 
 func getPointer() {
-	// GetPointer := GleamRT.MustFindProc("IMS_GetPointer")
+	GetPointer := GleamRT.MustFindProc("IMS_GetPointer")
+
+	id := int32(0)
+	var (
+		pointer *byte
+		size    uint32
+	)
+	ret, _, _ := GetPointer.Call(
+		uintptr(id), uintptr(unsafe.Pointer(&pointer)),
+		uintptr(unsafe.Pointer(&size)),
+	) // #nosec
+	if ret == 0 {
+		log.Fatalln("invalid value id")
+	}
+	fmt.Println("pointer:", pointer)
+	fmt.Println("size:", size)
+
+	data := unsafe.Slice(pointer, size) // #nosec
+	expected := []byte{0x01, 0x02, 0x03, 0x04}
+	if !bytes.Equal(expected, data) {
+		log.Fatalln("get value with incorrect data")
+	}
+	fmt.Println("get value:", data)
+}
+
+func deleteVal() {
+	Delete := GleamRT.MustFindProc("IMS_Delete")
+
+	id := int32(0)
+	ret, _, _ := Delete.Call(uintptr(id))
+	if ret == 0 {
+		log.Fatalln("invalid value id")
+	}
+
+	ret, _, _ = Delete.Call(uintptr(id))
+	if ret == 1 {
+		log.Fatalln("invalid value id")
+	}
+	fmt.Println("delete value:", id)
+}
+
+func deleteAll() {
+	DeleteAll := GleamRT.MustFindProc("IMS_DeleteAll")
+
+	ret, _, _ := DeleteAll.Call()
+	if ret == 0 {
+		log.Fatalln("failed to delete all data")
+	}
+	fmt.Println("delete all data")
 }
