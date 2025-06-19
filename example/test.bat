2dc560a8de0e579b01ef87bf48be2e766daf2fe6
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-06-19 14:37:32 +0800

    test.bat: build example about sleep, delete output exe files after test.

--- example/test.bat
+++ example/test.bat
@@ -1,7 +1,11 @@
 go build -v -trimpath -o GRT-PELoader.exe ../tool/pe_loader/main.go
 
-go build -v -trimpath -ldflags "-s -w" -o argument.exe argument/main.go
+go build -v -trimpath -ldflags "-s -w" -o argument.exe   argument/main.go
 go build -v -trimpath -ldflags "-s -w" -o im_storage.exe im_storage/main.go
+go build -v -trimpath -ldflags "-s -w" -o sleep.exe      sleep/main.go
 
 GRT-PELoader.exe -pe argument.exe
 GRT-PELoader.exe -pe im_storage.exe
+GRT-PELoader.exe -pe sleep.exe
+
+del /Q *.exe
