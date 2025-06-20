700cdef0de53aa48ea7ff73465a67f2da5dec3a3
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-06-20 18:10:35 +0800

    test.bat: build example about exit_process and watchdog.

--- example/test.bat
+++ example/test.bat
@@ -2,14 +2,18 @@
 
 go build -v -trimpath -o GRT-PELoader.exe ../tool/pe_loader/main.go
 
-go build -v -trimpath -ldflags "-s -w" -o argument.exe   argument/main.go
-go build -v -trimpath -ldflags "-s -w" -o im_storage.exe im_storage/main.go
-go build -v -trimpath -ldflags "-s -w" -o metrics.exe    metrics/main.go
-go build -v -trimpath -ldflags "-s -w" -o sleep.exe      sleep/main.go
+go build -v -trimpath -ldflags "-s -w" -o argument.exe     argument/main.go
+go build -v -trimpath -ldflags "-s -w" -o im_storage.exe   im_storage/main.go
+go build -v -trimpath -ldflags "-s -w" -o sleep.exe        sleep/main.go
+go build -v -trimpath -ldflags "-s -w" -o metrics.exe      metrics/main.go
+go build -v -trimpath -ldflags "-s -w" -o watchdog.exe     watchdog/main.go
+go build -v -trimpath -ldflags "-s -w" -o exit_process.exe exit_process/main.go
 
 GRT-PELoader.exe -pe argument.exe
 GRT-PELoader.exe -pe im_storage.exe
-GRT-PELoader.exe -pe metrics.exe
 GRT-PELoader.exe -pe sleep.exe
+GRT-PELoader.exe -pe metrics.exe
+GRT-PELoader.exe -pe watchdog.exe
+GRT-PELoader.exe -pe exit_process.exe
 
 del /Q *.exe
