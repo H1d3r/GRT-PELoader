cbd47fd6b882150b1a8dd9fb35826668384c8587
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-06-20 18:41:38 +0800

    mem_dll/main.go: add example for use the LoadInMemoryDLL.

---
+++ example/mem_dll/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"syscall"
+	"unsafe"
+
+	"github.com/davecgh/go-spew/spew"
+
+	"github.com/RSSU-Shellcode/GRT-PELoader/loader"
+)
+
+func main() {
+	image, err := os.ReadFile("C:\\Windows\\System32\\ws2_32.dll")
+	checkError(err)
+
+	instance, err := loader.LoadInMemoryDLL(image, nil)
+	checkError(err)
+	err = instance.Run()
+	checkError(err)
+
+	WSAStartup, err := instance.GetProcAddress("WSAStartup")
+	checkError(err)
+
+	var data syscall.WSAData
+	ret, _, _ := syscall.SyscallN(
+		WSAStartup, uintptr(0x202), uintptr(unsafe.Pointer(&data)),
+	)
+	fmt.Println(ret)
+	spew.Dump(data)
+
+	err = instance.Free()
+	checkError(err)
+}
+
+func checkError(err error) {
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
