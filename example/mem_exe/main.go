887bd782777990e1f1fbcfd993557b69856d7402
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-06-20 18:40:48 +0800

    mem_exe/main.go: add example for use the LoadInMemoryEXE.

---
+++ example/mem_exe/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/RSSU-Shellcode/GRT-PELoader/loader"
+)
+
+func main() {
+	image, err := os.ReadFile("example.exe")
+	checkError(err)
+
+	opts := loader.Options{
+		ImageName:   "test.exe",
+		CommandLine: "-p1 1 -p2 \"abc\"",
+	}
+	instance, err := loader.LoadInMemoryEXE(image, &opts)
+	checkError(err)
+
+	err = instance.Run()
+	checkError(err)
+
+	fmt.Println(instance.ExitCode())
+
+	err = instance.Free()
+	checkError(err)
+}
+
+func checkError(err error) {
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
