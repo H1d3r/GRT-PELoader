bb9c479fe56f5b5b77d9659cec06679f8914759b
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-06-21 14:52:38 +0800

    interact/main.go: add example for show how to interact with in-memory image.

---
+++ example/interact/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"os"
+
+	"github.com/RSSU-Shellcode/GRT-PELoader/loader"
+)
+
+func main() {
+	image, err := os.ReadFile("example.exe")
+	checkError(err)
+
+	input := bytes.NewBuffer(nil)
+	output := bytes.NewBuffer(nil)
+
+	input.WriteString("command\r\n")
+
+	opts := loader.Options{
+		ImageName:   "test.exe",
+		CommandLine: "-p1 1 -p2 \"abc\"",
+
+		Stdin:  input,
+		Stdout: output,
+		Stderr: output,
+	}
+	instance, err := loader.LoadInMemoryEXE(image, &opts)
+	checkError(err)
+	err = instance.Run()
+	checkError(err)
+	err = instance.Free()
+	checkError(err)
+
+	fmt.Println(output.String())
+}
+
+func checkError(err error) {
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
