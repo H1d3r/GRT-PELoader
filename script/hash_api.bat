ba0402a949c6532b5f264a0824f762843ca1ef38
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-10-05 12:35:13 +0800

    hash_api.bat: add section name for procedure, replace argument.

--- script/hash_api.bat
+++ script/hash_api.bat
@@ -6,151 +6,163 @@ echo =====================================================================
 echo.
 
 echo ------------------------x64------------------------
-hash_api -fmt 64 -conc -mod "kernel32.dll" -func VirtualAlloc
-hash_api -fmt 64 -conc -mod "kernel32.dll" -func VirtualFree
-hash_api -fmt 64 -conc -mod "kernel32.dll" -func VirtualProtect
-hash_api -fmt 64 -conc -mod "kernel32.dll" -func LoadLibraryA
-hash_api -fmt 64 -conc -mod "kernel32.dll" -func FreeLibrary
-hash_api -fmt 64 -conc -mod "kernel32.dll" -func GetProcAddress
-hash_api -fmt 64 -conc -mod "kernel32.dll" -func CreateThread
-hash_api -fmt 64 -conc -mod "kernel32.dll" -func ExitThread
-hash_api -fmt 64 -conc -mod "kernel32.dll" -func FlushInstructionCache
-hash_api -fmt 64 -conc -mod "kernel32.dll" -func CreateMutexA
-hash_api -fmt 64 -conc -mod "kernel32.dll" -func ReleaseMutex
-hash_api -fmt 64 -conc -mod "kernel32.dll" -func WaitForSingleObject
-hash_api -fmt 64 -conc -mod "kernel32.dll" -func CreateFileA
-hash_api -fmt 64 -conc -mod "kernel32.dll" -func CloseHandle
-hash_api -fmt 64 -conc -mod "kernel32.dll" -func GetCommandLineA
-hash_api -fmt 64 -conc -mod "kernel32.dll" -func GetCommandLineW
-hash_api -fmt 64 -conc -mod "kernel32.dll" -func LocalFree
-hash_api -fmt 64 -conc -mod "kernel32.dll" -func GetStdHandle
 
-hash_api -fmt 64 -conc -mod "kernel32.dll" -func GetProcAddress
-hash_api -fmt 64 -conc -mod "kernel32.dll" -func GetCommandLineA
-hash_api -fmt 64 -conc -mod "kernel32.dll" -func GetCommandLineW
-hash_api -fmt 64 -conc -mod "shell32.dll"  -func CommandLineToArgvW
-hash_api -fmt 64 -conc -mod "kernel32.dll" -func GetStdHandle
-hash_api -fmt 64 -conc -mod "kernel32.dll" -func CreateThread
-hash_api -fmt 64 -conc -mod "kernel32.dll" -func ExitThread
-hash_api -fmt 64 -conc -mod "kernel32.dll" -func ExitProcess
-hash_api -fmt 64 -conc -mod "ntdll.dll"    -func RtlExitUserThread
-hash_api -fmt 64 -conc -mod "ntdll.dll"    -func RtlExitUserProcess
-hash_api -fmt 64 -conc -mod "msvcrt.dll"   -func __getmainargs
-hash_api -fmt 64 -conc -mod "msvcrt.dll"   -func __wgetmainargs
-hash_api -fmt 64 -conc -mod "msvcrt.dll"   -func atexit
-hash_api -fmt 64 -conc -mod "msvcrt.dll"   -func _onexit
-hash_api -fmt 64 -conc -mod "msvcrt.dll"   -func _dllonexit
-hash_api -fmt 64 -conc -mod "msvcrt.dll"   -func exit
-hash_api -fmt 64 -conc -mod "msvcrt.dll"   -func _exit
-hash_api -fmt 64 -conc -mod "msvcrt.dll"   -func _Exit
-hash_api -fmt 64 -conc -mod "msvcrt.dll"   -func _cexit
-hash_api -fmt 64 -conc -mod "msvcrt.dll"   -func _c_exit
-hash_api -fmt 64 -conc -mod "msvcrt.dll"   -func quick_exit
-hash_api -fmt 64 -conc -mod "msvcrt.dll"   -func _amsg_exit
-hash_api -fmt 64 -conc -mod "msvcrt.dll"   -func _o_exit
-hash_api -fmt 64 -conc -mod "msvcrt.dll"   -func _beginthread
-hash_api -fmt 64 -conc -mod "msvcrt.dll"   -func _beginthreadex
-hash_api -fmt 64 -conc -mod "msvcrt.dll"   -func _endthread
-hash_api -fmt 64 -conc -mod "msvcrt.dll"   -func _endthreadex
-hash_api -fmt 64 -conc -mod "ucrtbase.dll" -func __p___argc
-hash_api -fmt 64 -conc -mod "ucrtbase.dll" -func __p___argv
-hash_api -fmt 64 -conc -mod "ucrtbase.dll" -func __p___wargv
-hash_api -fmt 64 -conc -mod "ucrtbase.dll" -func _crt_atexit
-hash_api -fmt 64 -conc -mod "ucrtbase.dll" -func _crt_at_quick_exit
-hash_api -fmt 64 -conc -mod "ucrtbase.dll" -func _register_onexit_function
-hash_api -fmt 64 -conc -mod "ucrtbase.dll" -func exit
-hash_api -fmt 64 -conc -mod "ucrtbase.dll" -func _exit
-hash_api -fmt 64 -conc -mod "ucrtbase.dll" -func _Exit
-hash_api -fmt 64 -conc -mod "ucrtbase.dll" -func _cexit
-hash_api -fmt 64 -conc -mod "ucrtbase.dll" -func _c_exit
-hash_api -fmt 64 -conc -mod "ucrtbase.dll" -func quick_exit
-hash_api -fmt 64 -conc -mod "ucrtbase.dll" -func _beginthread
-hash_api -fmt 64 -conc -mod "ucrtbase.dll" -func _beginthreadex
-hash_api -fmt 64 -conc -mod "ucrtbase.dll" -func _endthread
-hash_api -fmt 64 -conc -mod "ucrtbase.dll" -func _endthreadex
+echo [PE-Loader Core]
+hash_api -fmt 64 -conc -mod "kernel32.dll" -proc VirtualAlloc
+hash_api -fmt 64 -conc -mod "kernel32.dll" -proc VirtualFree
+hash_api -fmt 64 -conc -mod "kernel32.dll" -proc VirtualProtect
+hash_api -fmt 64 -conc -mod "kernel32.dll" -proc LoadLibraryA
+hash_api -fmt 64 -conc -mod "kernel32.dll" -proc FreeLibrary
+hash_api -fmt 64 -conc -mod "kernel32.dll" -proc GetProcAddress
+hash_api -fmt 64 -conc -mod "kernel32.dll" -proc CreateThread
+hash_api -fmt 64 -conc -mod "kernel32.dll" -proc ExitThread
+hash_api -fmt 64 -conc -mod "kernel32.dll" -proc FlushInstructionCache
+hash_api -fmt 64 -conc -mod "kernel32.dll" -proc CreateMutexA
+hash_api -fmt 64 -conc -mod "kernel32.dll" -proc ReleaseMutex
+hash_api -fmt 64 -conc -mod "kernel32.dll" -proc WaitForSingleObject
+hash_api -fmt 64 -conc -mod "kernel32.dll" -proc CreateFileA
+hash_api -fmt 64 -conc -mod "kernel32.dll" -proc CloseHandle
+hash_api -fmt 64 -conc -mod "kernel32.dll" -proc GetCommandLineA
+hash_api -fmt 64 -conc -mod "kernel32.dll" -proc GetCommandLineW
+hash_api -fmt 64 -conc -mod "kernel32.dll" -proc LocalFree
+hash_api -fmt 64 -conc -mod "kernel32.dll" -proc GetStdHandle
+echo.
 
-hash_api -fmt 64 -conc -mod "shell32.dll" -func CommandLineToArgvW
+echo [PE-Loader Hooks]
+hash_api -fmt 64 -conc -mod "kernel32.dll" -proc GetProcAddress
+hash_api -fmt 64 -conc -mod "kernel32.dll" -proc GetCommandLineA
+hash_api -fmt 64 -conc -mod "kernel32.dll" -proc GetCommandLineW
+hash_api -fmt 64 -conc -mod "shell32.dll"  -proc CommandLineToArgvW
+hash_api -fmt 64 -conc -mod "kernel32.dll" -proc GetStdHandle
+hash_api -fmt 64 -conc -mod "kernel32.dll" -proc CreateThread
+hash_api -fmt 64 -conc -mod "kernel32.dll" -proc ExitThread
+hash_api -fmt 64 -conc -mod "kernel32.dll" -proc ExitProcess
+hash_api -fmt 64 -conc -mod "ntdll.dll"    -proc RtlExitUserThread
+hash_api -fmt 64 -conc -mod "ntdll.dll"    -proc RtlExitUserProcess
+hash_api -fmt 64 -conc -mod "msvcrt.dll"   -proc __getmainargs
+hash_api -fmt 64 -conc -mod "msvcrt.dll"   -proc __wgetmainargs
+hash_api -fmt 64 -conc -mod "msvcrt.dll"   -proc atexit
+hash_api -fmt 64 -conc -mod "msvcrt.dll"   -proc _onexit
+hash_api -fmt 64 -conc -mod "msvcrt.dll"   -proc _dllonexit
+hash_api -fmt 64 -conc -mod "msvcrt.dll"   -proc exit
+hash_api -fmt 64 -conc -mod "msvcrt.dll"   -proc _exit
+hash_api -fmt 64 -conc -mod "msvcrt.dll"   -proc _Exit
+hash_api -fmt 64 -conc -mod "msvcrt.dll"   -proc _cexit
+hash_api -fmt 64 -conc -mod "msvcrt.dll"   -proc _c_exit
+hash_api -fmt 64 -conc -mod "msvcrt.dll"   -proc quick_exit
+hash_api -fmt 64 -conc -mod "msvcrt.dll"   -proc _amsg_exit
+hash_api -fmt 64 -conc -mod "msvcrt.dll"   -proc _o_exit
+hash_api -fmt 64 -conc -mod "msvcrt.dll"   -proc _beginthread
+hash_api -fmt 64 -conc -mod "msvcrt.dll"   -proc _beginthreadex
+hash_api -fmt 64 -conc -mod "msvcrt.dll"   -proc _endthread
+hash_api -fmt 64 -conc -mod "msvcrt.dll"   -proc _endthreadex
+hash_api -fmt 64 -conc -mod "ucrtbase.dll" -proc __p___argc
+hash_api -fmt 64 -conc -mod "ucrtbase.dll" -proc __p___argv
+hash_api -fmt 64 -conc -mod "ucrtbase.dll" -proc __p___wargv
+hash_api -fmt 64 -conc -mod "ucrtbase.dll" -proc _crt_atexit
+hash_api -fmt 64 -conc -mod "ucrtbase.dll" -proc _crt_at_quick_exit
+hash_api -fmt 64 -conc -mod "ucrtbase.dll" -proc _register_onexit_function
+hash_api -fmt 64 -conc -mod "ucrtbase.dll" -proc exit
+hash_api -fmt 64 -conc -mod "ucrtbase.dll" -proc _exit
+hash_api -fmt 64 -conc -mod "ucrtbase.dll" -proc _Exit
+hash_api -fmt 64 -conc -mod "ucrtbase.dll" -proc _cexit
+hash_api -fmt 64 -conc -mod "ucrtbase.dll" -proc _c_exit
+hash_api -fmt 64 -conc -mod "ucrtbase.dll" -proc quick_exit
+hash_api -fmt 64 -conc -mod "ucrtbase.dll" -proc _beginthread
+hash_api -fmt 64 -conc -mod "ucrtbase.dll" -proc _beginthreadex
+hash_api -fmt 64 -conc -mod "ucrtbase.dll" -proc _endthread
+hash_api -fmt 64 -conc -mod "ucrtbase.dll" -proc _endthreadex
+echo.
 
-hash_api -fmt 64 -conc -mod "msvcrt.dll" -func __getmainargs
-hash_api -fmt 64 -conc -mod "msvcrt.dll" -func __wgetmainargs
+echo [PE-Loader Misc]
+hash_api -fmt 64 -conc -mod "shell32.dll" -proc CommandLineToArgvW
 
-hash_api -fmt 64 -conc -mod "ucrtbase.dll" -func __p___argc
-hash_api -fmt 64 -conc -mod "ucrtbase.dll" -func __p___argv
-hash_api -fmt 64 -conc -mod "ucrtbase.dll" -func __p___wargv
+hash_api -fmt 64 -conc -mod "msvcrt.dll" -proc __getmainargs
+hash_api -fmt 64 -conc -mod "msvcrt.dll" -proc __wgetmainargs
+
+hash_api -fmt 64 -conc -mod "ucrtbase.dll" -proc __p___argc
+hash_api -fmt 64 -conc -mod "ucrtbase.dll" -proc __p___argv
+hash_api -fmt 64 -conc -mod "ucrtbase.dll" -proc __p___wargv
 echo.
 
 echo ------------------------x86------------------------
-hash_api -fmt 32 -conc -mod "kernel32.dll" -func VirtualAlloc
-hash_api -fmt 32 -conc -mod "kernel32.dll" -func VirtualFree
-hash_api -fmt 32 -conc -mod "kernel32.dll" -func VirtualProtect
-hash_api -fmt 32 -conc -mod "kernel32.dll" -func LoadLibraryA
-hash_api -fmt 32 -conc -mod "kernel32.dll" -func FreeLibrary
-hash_api -fmt 32 -conc -mod "kernel32.dll" -func GetProcAddress
-hash_api -fmt 32 -conc -mod "kernel32.dll" -func CreateThread
-hash_api -fmt 32 -conc -mod "kernel32.dll" -func ExitThread
-hash_api -fmt 32 -conc -mod "kernel32.dll" -func FlushInstructionCache
-hash_api -fmt 32 -conc -mod "kernel32.dll" -func CreateMutexA
-hash_api -fmt 32 -conc -mod "kernel32.dll" -func ReleaseMutex
-hash_api -fmt 32 -conc -mod "kernel32.dll" -func WaitForSingleObject
-hash_api -fmt 32 -conc -mod "kernel32.dll" -func CreateFileA
-hash_api -fmt 32 -conc -mod "kernel32.dll" -func CloseHandle
-hash_api -fmt 32 -conc -mod "kernel32.dll" -func GetCommandLineA
-hash_api -fmt 32 -conc -mod "kernel32.dll" -func GetCommandLineW
-hash_api -fmt 32 -conc -mod "kernel32.dll" -func LocalFree
-hash_api -fmt 32 -conc -mod "kernel32.dll" -func GetStdHandle
 
-hash_api -fmt 32 -conc -mod "kernel32.dll" -func GetProcAddress
-hash_api -fmt 32 -conc -mod "kernel32.dll" -func GetCommandLineA
-hash_api -fmt 32 -conc -mod "kernel32.dll" -func GetCommandLineW
-hash_api -fmt 32 -conc -mod "shell32.dll"  -func CommandLineToArgvW
-hash_api -fmt 32 -conc -mod "kernel32.dll" -func GetStdHandle
-hash_api -fmt 32 -conc -mod "kernel32.dll" -func CreateThread
-hash_api -fmt 32 -conc -mod "kernel32.dll" -func ExitThread
-hash_api -fmt 32 -conc -mod "kernel32.dll" -func ExitProcess
-hash_api -fmt 32 -conc -mod "ntdll.dll"    -func RtlExitUserThread
-hash_api -fmt 32 -conc -mod "ntdll.dll"    -func RtlExitUserProcess
-hash_api -fmt 32 -conc -mod "msvcrt.dll"   -func __getmainargs
-hash_api -fmt 32 -conc -mod "msvcrt.dll"   -func __wgetmainargs
-hash_api -fmt 32 -conc -mod "msvcrt.dll"   -func atexit
-hash_api -fmt 32 -conc -mod "msvcrt.dll"   -func _onexit
-hash_api -fmt 32 -conc -mod "msvcrt.dll"   -func _dllonexit
-hash_api -fmt 32 -conc -mod "msvcrt.dll"   -func exit
-hash_api -fmt 32 -conc -mod "msvcrt.dll"   -func _exit
-hash_api -fmt 32 -conc -mod "msvcrt.dll"   -func _Exit
-hash_api -fmt 32 -conc -mod "msvcrt.dll"   -func _cexit
-hash_api -fmt 32 -conc -mod "msvcrt.dll"   -func _c_exit
-hash_api -fmt 32 -conc -mod "msvcrt.dll"   -func quick_exit
-hash_api -fmt 32 -conc -mod "msvcrt.dll"   -func _amsg_exit
-hash_api -fmt 32 -conc -mod "msvcrt.dll"   -func _o_exit
-hash_api -fmt 32 -conc -mod "msvcrt.dll"   -func _beginthread
-hash_api -fmt 32 -conc -mod "msvcrt.dll"   -func _beginthreadex
-hash_api -fmt 32 -conc -mod "msvcrt.dll"   -func _endthread
-hash_api -fmt 32 -conc -mod "msvcrt.dll"   -func _endthreadex
-hash_api -fmt 32 -conc -mod "ucrtbase.dll" -func __p___argc
-hash_api -fmt 32 -conc -mod "ucrtbase.dll" -func __p___argv
-hash_api -fmt 32 -conc -mod "ucrtbase.dll" -func __p___wargv
-hash_api -fmt 32 -conc -mod "ucrtbase.dll" -func _crt_atexit
-hash_api -fmt 32 -conc -mod "ucrtbase.dll" -func _crt_at_quick_exit
-hash_api -fmt 32 -conc -mod "ucrtbase.dll" -func _register_onexit_function
-hash_api -fmt 32 -conc -mod "ucrtbase.dll" -func exit
-hash_api -fmt 32 -conc -mod "ucrtbase.dll" -func _exit
-hash_api -fmt 32 -conc -mod "ucrtbase.dll" -func _Exit
-hash_api -fmt 32 -conc -mod "ucrtbase.dll" -func _cexit
-hash_api -fmt 32 -conc -mod "ucrtbase.dll" -func _c_exit
-hash_api -fmt 32 -conc -mod "ucrtbase.dll" -func quick_exit
-hash_api -fmt 32 -conc -mod "ucrtbase.dll" -func _beginthread
-hash_api -fmt 32 -conc -mod "ucrtbase.dll" -func _beginthreadex
-hash_api -fmt 32 -conc -mod "ucrtbase.dll" -func _endthread
-hash_api -fmt 32 -conc -mod "ucrtbase.dll" -func _endthreadex
+echo [PE-Loader Core]
+hash_api -fmt 32 -conc -mod "kernel32.dll" -proc VirtualAlloc
+hash_api -fmt 32 -conc -mod "kernel32.dll" -proc VirtualFree
+hash_api -fmt 32 -conc -mod "kernel32.dll" -proc VirtualProtect
+hash_api -fmt 32 -conc -mod "kernel32.dll" -proc LoadLibraryA
+hash_api -fmt 32 -conc -mod "kernel32.dll" -proc FreeLibrary
+hash_api -fmt 32 -conc -mod "kernel32.dll" -proc GetProcAddress
+hash_api -fmt 32 -conc -mod "kernel32.dll" -proc CreateThread
+hash_api -fmt 32 -conc -mod "kernel32.dll" -proc ExitThread
+hash_api -fmt 32 -conc -mod "kernel32.dll" -proc FlushInstructionCache
+hash_api -fmt 32 -conc -mod "kernel32.dll" -proc CreateMutexA
+hash_api -fmt 32 -conc -mod "kernel32.dll" -proc ReleaseMutex
+hash_api -fmt 32 -conc -mod "kernel32.dll" -proc WaitForSingleObject
+hash_api -fmt 32 -conc -mod "kernel32.dll" -proc CreateFileA
+hash_api -fmt 32 -conc -mod "kernel32.dll" -proc CloseHandle
+hash_api -fmt 32 -conc -mod "kernel32.dll" -proc GetCommandLineA
+hash_api -fmt 32 -conc -mod "kernel32.dll" -proc GetCommandLineW
+hash_api -fmt 32 -conc -mod "kernel32.dll" -proc LocalFree
+hash_api -fmt 32 -conc -mod "kernel32.dll" -proc GetStdHandle
+echo.
 
-hash_api -fmt 32 -conc -mod "shell32.dll" -func CommandLineToArgvW
+echo [PE-Loader Hooks]
+hash_api -fmt 32 -conc -mod "kernel32.dll" -proc GetProcAddress
+hash_api -fmt 32 -conc -mod "kernel32.dll" -proc GetCommandLineA
+hash_api -fmt 32 -conc -mod "kernel32.dll" -proc GetCommandLineW
+hash_api -fmt 32 -conc -mod "shell32.dll"  -proc CommandLineToArgvW
+hash_api -fmt 32 -conc -mod "kernel32.dll" -proc GetStdHandle
+hash_api -fmt 32 -conc -mod "kernel32.dll" -proc CreateThread
+hash_api -fmt 32 -conc -mod "kernel32.dll" -proc ExitThread
+hash_api -fmt 32 -conc -mod "kernel32.dll" -proc ExitProcess
+hash_api -fmt 32 -conc -mod "ntdll.dll"    -proc RtlExitUserThread
+hash_api -fmt 32 -conc -mod "ntdll.dll"    -proc RtlExitUserProcess
+hash_api -fmt 32 -conc -mod "msvcrt.dll"   -proc __getmainargs
+hash_api -fmt 32 -conc -mod "msvcrt.dll"   -proc __wgetmainargs
+hash_api -fmt 32 -conc -mod "msvcrt.dll"   -proc atexit
+hash_api -fmt 32 -conc -mod "msvcrt.dll"   -proc _onexit
+hash_api -fmt 32 -conc -mod "msvcrt.dll"   -proc _dllonexit
+hash_api -fmt 32 -conc -mod "msvcrt.dll"   -proc exit
+hash_api -fmt 32 -conc -mod "msvcrt.dll"   -proc _exit
+hash_api -fmt 32 -conc -mod "msvcrt.dll"   -proc _Exit
+hash_api -fmt 32 -conc -mod "msvcrt.dll"   -proc _cexit
+hash_api -fmt 32 -conc -mod "msvcrt.dll"   -proc _c_exit
+hash_api -fmt 32 -conc -mod "msvcrt.dll"   -proc quick_exit
+hash_api -fmt 32 -conc -mod "msvcrt.dll"   -proc _amsg_exit
+hash_api -fmt 32 -conc -mod "msvcrt.dll"   -proc _o_exit
+hash_api -fmt 32 -conc -mod "msvcrt.dll"   -proc _beginthread
+hash_api -fmt 32 -conc -mod "msvcrt.dll"   -proc _beginthreadex
+hash_api -fmt 32 -conc -mod "msvcrt.dll"   -proc _endthread
+hash_api -fmt 32 -conc -mod "msvcrt.dll"   -proc _endthreadex
+hash_api -fmt 32 -conc -mod "ucrtbase.dll" -proc __p___argc
+hash_api -fmt 32 -conc -mod "ucrtbase.dll" -proc __p___argv
+hash_api -fmt 32 -conc -mod "ucrtbase.dll" -proc __p___wargv
+hash_api -fmt 32 -conc -mod "ucrtbase.dll" -proc _crt_atexit
+hash_api -fmt 32 -conc -mod "ucrtbase.dll" -proc _crt_at_quick_exit
+hash_api -fmt 32 -conc -mod "ucrtbase.dll" -proc _register_onexit_function
+hash_api -fmt 32 -conc -mod "ucrtbase.dll" -proc exit
+hash_api -fmt 32 -conc -mod "ucrtbase.dll" -proc _exit
+hash_api -fmt 32 -conc -mod "ucrtbase.dll" -proc _Exit
+hash_api -fmt 32 -conc -mod "ucrtbase.dll" -proc _cexit
+hash_api -fmt 32 -conc -mod "ucrtbase.dll" -proc _c_exit
+hash_api -fmt 32 -conc -mod "ucrtbase.dll" -proc quick_exit
+hash_api -fmt 32 -conc -mod "ucrtbase.dll" -proc _beginthread
+hash_api -fmt 32 -conc -mod "ucrtbase.dll" -proc _beginthreadex
+hash_api -fmt 32 -conc -mod "ucrtbase.dll" -proc _endthread
+hash_api -fmt 32 -conc -mod "ucrtbase.dll" -proc _endthreadex
+echo.
 
-hash_api -fmt 32 -conc -mod "msvcrt.dll" -func __getmainargs
-hash_api -fmt 32 -conc -mod "msvcrt.dll" -func __wgetmainargs
+echo [PE-Loader Misc]
+hash_api -fmt 32 -conc -mod "shell32.dll" -proc CommandLineToArgvW
 
-hash_api -fmt 32 -conc -mod "ucrtbase.dll" -func __p___argc
-hash_api -fmt 32 -conc -mod "ucrtbase.dll" -func __p___argv
-hash_api -fmt 32 -conc -mod "ucrtbase.dll" -func __p___wargv
+hash_api -fmt 32 -conc -mod "msvcrt.dll" -proc __getmainargs
+hash_api -fmt 32 -conc -mod "msvcrt.dll" -proc __wgetmainargs
+
+hash_api -fmt 32 -conc -mod "ucrtbase.dll" -proc __p___argc
+hash_api -fmt 32 -conc -mod "ucrtbase.dll" -proc __p___argv
+hash_api -fmt 32 -conc -mod "ucrtbase.dll" -proc __p___wargv
 echo.
 
 pause
