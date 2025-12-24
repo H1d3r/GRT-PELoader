9bc917c0fd82cac11b9f74da68445fe9bf4c0e55
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-12-24 13:39:48 +0800

    exit_process/main.go: rewrite for only use gleamrt.ExitProcess.

--- example/exit_process/main.go
+++ example/exit_process/main.go
@@ -1,26 +1,9 @@
 package main
 
 import (
-	"golang.org/x/sys/windows"
+	"github.com/RSSU-Shellcode/Gleam-RT/runtime"
 )
 
-// GleamRT is a virtual dll for get runtime methods.
-var GleamRT *windows.DLL
-
-func init() {
-	var err error
-	GleamRT, err = windows.LoadDLL("GleamRT.dll")
-	if err != nil {
-		panic("failed to load virtual runtime dll")
-	}
-}
-
 func main() {
-	exitProcess(1)
-}
-
-func exitProcess(code int) {
-	ExitProcess := GleamRT.MustFindProc("ExitProcess")
-
-	_, _, _ = ExitProcess.Call(uintptr(code))
+	gleamrt.ExitProcess(0)
 }
