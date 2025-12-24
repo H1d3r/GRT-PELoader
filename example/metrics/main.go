577cfa1a135b71dd3f935f4cd8af6a178124a4df
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-12-24 13:37:08 +0800

    metrics/main.go: rewrite for use package Gleam-RT/runtime.

--- example/metrics/main.go
+++ example/metrics/main.go
@@ -2,32 +2,16 @@ package main
 
 import (
 	"log"
-	"unsafe"
 
 	"github.com/davecgh/go-spew/spew"
-	"golang.org/x/sys/windows"
 
-	"github.com/RSSU-Shellcode/GRT-Develop/metric"
+	"github.com/RSSU-Shellcode/Gleam-RT/runtime"
 )
 
-// GleamRT is a virtual dll for get runtime methods.
-var GleamRT *windows.DLL
-
-func init() {
-	var err error
-	GleamRT, err = windows.LoadDLL("GleamRT.dll")
-	if err != nil {
-		panic("failed to load virtual runtime dll")
-	}
-}
-
 func main() {
-	GetMetrics := GleamRT.MustFindProc("GetMetrics")
-
-	metrics := metric.Metrics{}
-	ret, _, _ := GetMetrics.Call(uintptr(unsafe.Pointer(&metrics))) // #nosec
-	if ret != 0 {
-		log.Fatalf("failed to call GetMetrics: 0x%X", ret)
+	metrics, err := gleamrt.GetMetrics()
+	if err != nil {
+		log.Fatal(err)
 	}
 	spew.Dump(metrics)
 }
