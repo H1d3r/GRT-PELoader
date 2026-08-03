601c47458fd5930ce7b9f20d4b140d630bf7ea2a
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-08-03 12:58:59 +0800

    win_api.h: add IsValidModuleHandle and GetProcedureName.

--- include/win_api.h
+++ include/win_api.h
@@ -3,9 +3,23 @@
 
 #include "c_types.h"
 #include "win_types.h"
+#include "win_structs.h"
 #include "dll_kernel32.h"
+#include "hash_api.h"
 
-DWORD   GetModuleFileName(void* list, HMODULE hModule, LPWSTR lpFilename, DWORD nSize);
-HMODULE GetModuleHandle(void* list, LPWSTR lpFilename);
+// some function names have been slightly modified
+// to avoid conflicts with kernel32.lib.
+
+// IsValidModuleHandle is used to check module handle is valid.
+BOOL IsValidModuleHandle(PML* pml, HMODULE hModule);
+
+// GetModuleBaseName is used to get module base name by handle.
+DWORD GetModuleBaseName(PML* pml, HMODULE hModule, LPWSTR lpBasename, DWORD nSize);
+
+// GetModuleHandle is used to get module handle by base name.
+HMODULE GetModuleHandle(PML* pml, LPWSTR lpBasename);
+
+// GetProcedureName is used to get procedure name by address.
+LPSTR GetProcedureName(PML* pml, HMODULE hModule, void* procedure);
 
 #endif // WIN_API_H
