469e3fd01225d6db8cd5baa598d9aba9458e7bfe
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-08-03 12:59:37 +0800

    win_api.c: refactor implement about import PML, add new methods.

--- src/win_api.c
+++ src/win_api.c
@@ -1,72 +1,151 @@
 #include "c_types.h"
 #include "win_types.h"
+#include "win_structs.h"
 #include "dll_kernel32.h"
 #include "lib_memory.h"
 #include "lib_string.h"
+#include "hash_api.h"
+#include "pe_image.h"
 #include "win_api.h"
 
-DWORD GetModuleFileName(void* list, HMODULE hModule, LPWSTR lpFilename, DWORD nSize)
+static LPSTR getProcedureName(HMODULE hModule, void* procedure);
+
+BOOL IsValidModuleHandle(PML* pml, HMODULE hModule)
 {
-    uintptr mod = (uintptr)list;
-    for (;; mod = *(uintptr*)(mod))
+    if (hModule == NULL)
     {
-    #ifdef _WIN64
-        uintptr modName = *(uintptr*)(mod + 80);
-    #elif _WIN32
-        uintptr modName = *(uintptr*)(mod + 40);
-    #endif
-        if (modName == 0x00)
+        return false;
+    }
+    LIST_ENTRY* head = &pml->Links;
+    for (LIST_ENTRY* link = head->Flink; link != head; link = link->Flink)
+    {
+        PML* entry = (PML*)((uintptr)(link)-offsetof(PML, Links));
+        if (entry->ImageBase == hModule)
         {
-            break;
+            return true;
         }
-    #ifdef _WIN64
-        uintptr modBase = *(uintptr*)(mod + 32);
-    #elif _WIN32
-        uintptr modBase = *(uintptr*)(mod + 16);
-    #endif
-        if (modBase != (uintptr)hModule)
+    }
+    return false;
+}
+
+DWORD GetModuleBaseName(PML* pml, HMODULE hModule, LPWSTR lpBasename, DWORD nSize)
+{
+    if (hModule == NULL || lpBasename == NULL || nSize == 0)
+    {
+        return 0;
+    }
+    LIST_ENTRY* head = &pml->Links;
+    for (LIST_ENTRY* link = head->Flink; link != head; link = link->Flink)
+    {
+        PML* entry = (PML*)((uintptr)(link)-offsetof(PML, Links));
+        if (entry->ImageBase != hModule)
         {
             continue;
         }
-    #ifdef _WIN64
-        uint16 nameLen = *(uint16*)(mod + 74);
-    #elif _WIN32
-        uint16 nameLen = *(uint16*)(mod + 38);
-    #endif
-        if (nameLen > nSize)
+        LPWSTR nameBuf = entry->BaseName.Buffer;
+        USHORT nameLen = entry->BaseName.Length/2;
+        if (nameLen >= nSize)
         {
-            nameLen = (uint16)nSize;
+            nameLen = (USHORT)(nSize-1);
         }
-        mem_copy(lpFilename, (LPWSTR)modName, nameLen);
+        strncpy_w(lpBasename, nameBuf, nameLen);
+        lpBasename[nameLen] = 0x0000;
         return nameLen;
     }
     return 0;
 }
 
-HMODULE GetModuleHandle(void* list, LPWSTR lpFilename)
+HMODULE GetModuleHandle(PML* pml, LPWSTR lpBasename)
 {
-    uintptr mod = (uintptr)list;
-    for (;; mod = *(uintptr*)(mod))
+    if (lpBasename == NULL)
     {
-    #ifdef _WIN64
-        uintptr modName = *(uintptr*)(mod + 80);
-    #elif _WIN32
-        uintptr modName = *(uintptr*)(mod + 40);
-    #endif
-        if (modName == 0x00)
-        {
-            break;
-        }
-        if (stricmp_w((LPWSTR)modName, lpFilename) != 0)
+        return NULL;
+    }
+    uint len = strlen_w(lpBasename);
+    LIST_ENTRY* head = &pml->Links;
+    for (LIST_ENTRY* link = head->Flink; link != head; link = link->Flink)
+    {
+        PML* entry = (PML*)((uintptr)(link)-offsetof(PML, Links));
+        LPWSTR nameBuf = entry->BaseName.Buffer;
+        USHORT nameLen = entry->BaseName.Length/2;
+        if (nameLen != len)
         {
             continue;
         }
-    #ifdef _WIN64
-        uintptr modBase = *(uintptr*)(mod + 32);
-    #elif _WIN32
-        uintptr modBase = *(uintptr*)(mod + 16);
-    #endif
-        return (HMODULE)modBase;
+        if (strnicmp_w(lpBasename, nameBuf, nameLen) == 0)
+        {
+            return entry->ImageBase;
+        }
+    }
+    return NULL;
+}
+
+LPSTR GetProcedureName(PML* pml, HMODULE hModule, void* procedure)
+{
+    if (procedure == NULL)
+    {
+        return NULL;
+    }
+    // try to get procedure name from self module first
+    if (hModule != NULL)
+    {
+        LPSTR name = getProcedureName(hModule, procedure);
+        if (name != NULL)
+        {
+            return name;
+        }
+    }
+    // if not exist, search procedure name from pml.
+    // like forwarded procedure in EAT.
+    LIST_ENTRY* head = &pml->Links;
+    for (LIST_ENTRY* link = head->Flink; link != head; link = link->Flink)
+    {
+        PML* entry = (PML*)((uintptr)(link)-offsetof(PML, Links));
+        LPSTR name = getProcedureName(entry->ImageBase, procedure);
+        if (name != NULL)
+        {
+            return name;
+        }
+    }
+    return NULL;
+}
+
+static LPSTR getProcedureName(HMODULE hModule, void* procedure)
+{
+    uintptr proc = (uintptr)procedure;
+    // check image base address is expected
+    uintptr base = (uintptr)(hModule);
+    // parse module information
+    PE_Image image;
+    mem_init(&image, sizeof(image));
+    ParsePEImage(hModule, &image);
+    // calculate the text section range
+    uintptr begin = base + image.Text.VirtualAddress;
+    uintptr end = begin + image.Text.VirtualSize;
+    if (proc < begin || proc > end)
+    {
+        return NULL;
+    }
+    // get RVA of export address tables(EAT)
+    Image_DataDirectory* DD = &image.OptionalHeader.DataDirectory[0];
+    Image_DataDirectory  EAT = DD[IMAGE_DIRECTORY_ENTRY_EXPORT];
+    // get export directory structure
+    Image_ExportDirectory* dir = (Image_ExportDirectory*)(base + EAT.VirtualAddress);
+    // process EAT arrays
+    uint32* nameTable = (uint32*)(base + dir->AddressOfNames);
+    uint32* funcTable = (uint32*)(base + dir->AddressOfFunctions);
+    uint16* ordiTable = (uint16*)(base + dir->AddressOfNameOrdinals);
+    // compare the exported function address
+    for (uint32 i = 0; i < dir->NumberOfNames; i++)
+    {
+        // name[i] -> ordinal[i] -> funcRVA[ordinal]
+        uint32 funcRVA = funcTable[ordiTable[i]];
+        if (base + funcRVA == proc)
+        {
+            // get procedure name address
+            byte* procName = (byte*)(base + nameTable[i]);
+            return procName;
+        }
     }
     return NULL;
 }
