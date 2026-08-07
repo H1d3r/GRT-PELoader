ccb1ef610fd17226b64548971f302eb4a6bf3178
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-08-07 13:01:48 +0800

    debug.h: add dbg_lock and dbg_unlock for multi thread.

--- include/debug.h
+++ include/debug.h
@@ -21,11 +21,17 @@
 
 bool InitDebugger();
 
+void dbg_lock();
+void dbg_unlock();
+
 void dbg_log(char* mod, char* fmt, ...);
 
 #else
 
 #define InitDebugger() (true)
+
+#define dbg_lock()
+#define dbg_unlock()
 
 #define dbg_log(mod, fmt, ...)
 
