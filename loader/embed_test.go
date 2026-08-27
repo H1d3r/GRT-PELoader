1b171a7f922a361d6c8936ef6a836ab5c181e62c
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-08-27 14:20:42 +0800

    embed_test.go: update tests about options, rewrite TestEmbedInstance.

--- loader/embed_test.go
+++ loader/embed_test.go
@@ -16,7 +16,7 @@ func TestEmbed(t *testing.T) {
 	require.NoError(t, err)
 
 	t.Run("common", func(t *testing.T) {
-		embed := NewEmbed(image)
+		embed := NewEmbed(image, nil)
 
 		config, err := embed.Encode()
 		require.NoError(t, err)
@@ -26,7 +26,8 @@ func TestEmbed(t *testing.T) {
 	})
 
 	t.Run("invalid PE image", func(t *testing.T) {
-		embed := NewEmbed([]byte{0x00, 0x01})
+		invalid := []byte{0x00, 0x01}
+		embed := NewEmbed(invalid, nil)
 
 		config, err := embed.Encode()
 		require.EqualError(t, err, "invalid PE image: EOF")
@@ -34,7 +35,7 @@ func TestEmbed(t *testing.T) {
 	})
 
 	t.Run("mode", func(t *testing.T) {
-		embed := NewEmbed(image)
+		embed := NewEmbed(image, nil)
 		require.Equal(t, ModeEmbed, embed.Mode())
 	})
 }
@@ -44,7 +45,12 @@ func TestEmbedCompress(t *testing.T) {
 	require.NoError(t, err)
 
 	t.Run("common", func(t *testing.T) {
-		embed := NewEmbedCompress(image, 4096)
+		opts := EmbedOptions{
+			Compress:   true,
+			WindowSize: lzss.MaximumWindowSize,
+			ChainLen:   lzss.DefaultChainLen,
+		}
+		embed := NewEmbed(image, &opts)
 
 		config, err := embed.Encode()
 		require.NoError(t, err)
@@ -54,7 +60,12 @@ func TestEmbedCompress(t *testing.T) {
 	})
 
 	t.Run("invalid window size", func(t *testing.T) {
-		embed := NewEmbedCompress(image, 40960)
+		opts := EmbedOptions{
+			Compress:   true,
+			WindowSize: 40960,
+			ChainLen:   lzss.DefaultChainLen,
+		}
+		embed := NewEmbed(image, &opts)
 
 		config, err := embed.Encode()
 		errStr := "failed to compress PE image: invalid window size"
@@ -63,15 +74,20 @@ func TestEmbedCompress(t *testing.T) {
 	})
 }
 
-func TestEmbedPreCompress(t *testing.T) {
+func TestEmbedPreCompressed(t *testing.T) {
 	image, err := os.ReadFile("testdata/executable.dat")
 	require.NoError(t, err)
 
 	t.Run("common", func(t *testing.T) {
-		compressed, err := lzss.Compress(image, 4096)
+		windowSize := lzss.MaximumWindowSize
+		chainLen := lzss.DefaultChainLen
+		compressed, err := lzss.Compress(image, windowSize, chainLen)
 		require.NoError(t, err)
 
-		embed := NewEmbedPreCompress(compressed)
+		opts := EmbedOptions{
+			PreCompressed: true,
+		}
+		embed := NewEmbed(compressed, &opts)
 
 		config, err := embed.Encode()
 		require.NoError(t, err)
@@ -91,22 +107,39 @@ func TestEmbedInstance(t *testing.T) {
 			path := filepath.Join(dir, item)
 			image, err := os.ReadFile(path)
 			require.NoError(t, err)
-			opts := &Options{
-				ImageName:    "test.exe",
-				CommandLine:  "-p1 123 -p2 \"hello\"",
-				WaitMain:     true,
-				AllowSkipDLL: true,
-			}
 
-			preCompressed, err := lzss.Compress(image, 2048)
+			windowSize := lzss.MaximumWindowSize
+			chainLen := lzss.DefaultChainLen
+			preCompressed, err := lzss.Compress(image, windowSize, chainLen)
 			require.NoError(t, err)
-			embed1 := NewEmbed(image)
-			embed2 := NewEmbedCompress(image, 2048)
-			embed3 := NewEmbedPreCompress(preCompressed)
+
+			opts := &EmbedOptions{
+				Compress: false,
+			}
+			embed1 := NewEmbed(image, opts)
+
+			opts = &EmbedOptions{
+				Compress:   true,
+				WindowSize: lzss.MaximumWindowSize,
+				ChainLen:   lzss.DefaultChainLen,
+			}
+			embed2 := NewEmbed(image, opts)
+
+			opts = &EmbedOptions{
+				PreCompressed: true,
+			}
+			embed3 := NewEmbed(preCompressed, opts)
 
 			for _, img := range []Image{
 				embed1, embed2, embed3,
 			} {
+				opts := &Options{
+					ImageName:    "test.exe",
+					CommandLine:  "-p1 123 -p2 \"hello\"",
+					WaitMain:     true,
+					AllowSkipDLL: true,
+				}
+
 				inst, err := CreateInstance(runtime.GOARCH, img, opts)
 				require.NoError(t, err)
 
